50f5d2ede64ee1a2f71d53eb6a4a02637ecd2f77
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-18 12:49:28 -0300

    alertas da escolha do numero

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('boas vindas ao jogo do numero secreto');
+let numeroSecreto = 5;
+let escolha = prompt('escolha um número entre 1 e 10 ');
+
+if(ecolha == numeroSecreto) {
+    console.log(você acertou)
+}
