2b9811ad5062147f09ffa8f6a640ebfbf52e6dbe
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-18 13:41:14 -0300

    condiçoes e comentarios

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('boas vindas ao jogo do numero secreto');
 let numeroSecreto = 5;
 let escolha = prompt('escolha um número entre 1 e 10 ');
+console.log(numeroSecreto)
 
 if(ecolha == numeroSecreto) {
-    console.log(você acertou)
+    alert(`você acertou ${numeroSecreto}`)
+} else {
+    alert('você errou')
 }
