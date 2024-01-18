33eb9dcc2fc38847dbeb01636242818e7ac32909
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-18 16:06:43 -0300

    numero random

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 alert('boas vindas ao jogo do numero secreto.');
-let numeroSecreto = 5;
+let numeroMaximo = parseInt(Math.random() * 1000 + 1)
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto){
-    chute = prompt('escolha um número 1 e 10.');
+    chute = prompt(`escolha um número 1 e ${numeroMaximo}.`);
     if(chute == numeroSecreto){
         break;
     } else {
